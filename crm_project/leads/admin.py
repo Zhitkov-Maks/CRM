457a9bcf3bb42f38b1d4ef20113f9a7b64264de3
Maksim <m-zhitkov@inbox.ru>
2024-04-18 21:50:14 +0300

    Connected the application leads

---
+++ crm_project/leads/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Leads
+
+
+@admin.register(Leads)
+class LeadsAdmin(admin.ModelAdmin):
+    """Настраиваем админ панель для потенциальных клиентов."""
+    list_display = (
+        "pk",
+        "last_name",
+        "first_name",
+        "middle_name",
+        "email",
+        "phone",
+        "advertising"
+    )
+    list_display_links = ("last_name", "first_name")
+    search_fields = ("last_name",)
+    autocomplete_fields = ("advertising",)
