58938c64ce121d2739e06a9ab3ec1c8f16e0cdb8
Maksim <m-zhitkov@inbox.ru>
2024-04-20 22:30:07 +0300

    Corrected the display in the admin panel

--- crm_project/advertise/admin.py
+++ crm_project/advertise/admin.py
@@ -7,10 +7,10 @@ class AdvertiseAdmin(admin.ModelAdmin):
     """Настройки админки для рекламных кампаний."""
     list_display: tuple = ('id', 'name', 'product', 'budget')
     list_display_links: tuple = ('id', 'name')
-    search_fields: tuple = ('name',)
     list_editable: tuple = ('budget',)
     list_select_related: bool = True
-    autocomplete_fields: tuple = ("product",)
+    autocomplete_fields: tuple = ('product',)
+    search_fields: tuple = ('name',)
 
 
 @admin.register(PromotionChannel)
